3f71d68298d511ae5bccf3d528da7c7c22279152
AmineBotza <botza.amine@gmail.com>
2023-10-07 00:48:27 +0100

    creating a c file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+*main - entry point
+*puts - printing header
+*return - alwalys 0
+*/
+int main(void)
+{
+	puts("\""Programming is like building a multilingual puzzle") ;
+	return 0 ;
+}
