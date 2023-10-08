3845ef315fa38b15f63b773b4de7f53f1699540f
AmineBotza <botza.amine@gmail.com>
2023-10-08 03:32:15 +0100

    displaying alphabet using putchar and while loop

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+  *main - Entry point
+  *
+  *Description: this function types alphabet in lower case.
+  *
+  *Return: Always 0 (success)
+  */
+  int main(void)
+{
+	int n = 97;
+	
+	while (n<=122)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
