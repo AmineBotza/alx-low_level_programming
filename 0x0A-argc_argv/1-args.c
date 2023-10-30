01e41f79c2fc625e581594538749e8e18f0eee33
AmineBotza <botza.amine@gmail.com>
2023-10-30 23:02:29 +0100

    printing number of arguments

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,20 +1,14 @@
 #include <stdio.h>
 /**
   *main - Entry point
-  *@argc: Counts th number of arguments
+  *@argc: input
   *@argv: Vector of array argument
   *
   *Return: Always 0 (success)
   */
 int main(int argc, char *argv[])
 {
-	if (argc == 1)
-	{
-		printf("0\n");
-	}
-	else
-	{
-		printf("%d\n", argc - 1);
-	}
+	(void)argv;
+	printf("%d\n", (argc - 1));
 	return (0);
 }
