3d0772c1b300c2ac4418071d682286f7ef607e88
AmineBotza <botza.amine@gmail.com>
2023-11-01 23:39:39 +0100

    library

---
+++ 0x09-static_libraries/4-strpbrk.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * _strpbrk - Entry point
+ * @s: input
+ * @accept: input
+ * Return: Always 0 (Success)
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int a = 0, b;
+
+	while (s[a])
+	{
+		b = 0;
+
+		while (accept[b])
+		{
+			if (s[a] == accept[b])
+			{
+				s += a;
+				return (s);
+			}
+
+			b++;
+		}
+
+		a++;
+	}
+
+	return (0);
+}
